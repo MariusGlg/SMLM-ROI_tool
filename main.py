358f1c88ed0ee52ded8569f38f5473da056a5e10
mglogger <marius.glogger@googlemail.com>
2024-03-28 10:35:16 +0100

    added snipping of localzations files (not yet dbscan analyzed) next to dbscan/clustered and dbcluster/cluster_centers file

--- main.py
+++ main.py
@@ -49,8 +49,8 @@ header_dbscan = ["frame", "x", "y", "photons", "sx", "sy", "bg", "lpx", "lpy", "
 header_dbscluster = ["groups", "convex_hull", "area", "mean_frame", "com_x", "com_y", "std_frame",
                  "std_x", "std_y", "n"]  # column header of dbscluster file (len = 10)
 
-x_coor = ()
-y_coor = ()
+#"x" = ()
+#"y" = ()
 if Mask == "True":
     appendix = "_Mask"
 else:
@@ -83,37 +83,40 @@ class Save(object):
         self.filename = filename
         self.polygon = polygon
 
-    def save_pd_to_hdf5(self):
+    def getformats(self):
+        # get formats of hdf5 files (locs, dbscan/clustered, dbcluster/cluster_centers
+        formats = ([(np.float32)] * (len(column_names)))
+        if len(column_names) == 14:  # locs file
+            formats[0] = (np.uint32)
+            formats[11] = (np.uint32)
+            formats[12] = (np.uint32)
+        if len(column_names) == 15:  # dbscan/clustered file
+            formats[0] = (np.uint32)
+            formats[11] = (np.uint32)
+            formats[12] = (np.uint32)
+            formats[14] = (int)
+        if len(column_names) == 18:  # dbcluster/cluster_centers file
+            #14 16 17
+            formats[14] = (int)
+            formats[16] = (np.uint32)
+            formats[17] = (int)
+        return formats
+
+
+    def save_pd_to_hdf5(self, formats):
         p1_dbscan_filtered = self.p_coloc.values.tolist()  # convert to list of lists
         p1_dbscan_filtered = [tuple(x) for x in p1_dbscan_filtered]  # convert to list of tuples
         name = self.filename + "_" + appendix +".hdf5"
 
-        if x_coor == "com_x":
-            #generates picasso-readable h5py dbcluster files
-            with h5py.File(os.path.join(self.path, str(name)), "w") as locs_file:
-                # define column types for locs file
-                formats = ([(np.float32)] * (len(header_dbscluster)))
-                formats[0] = (int)
-                formats[9] = (np.uint32)
-                ds_dt = np.dtype({'names': header_dbscluster, 'formats': formats})
-                locs_file.create_dataset("locs", data=p1_dbscan_filtered, dtype=ds_dt)
-            #define column times for dbscan file
-        else:
-            # generates picasso-readable h5py dbscan files
-            with h5py.File(os.path.join(self.path, str(name)), "w") as locs_file:
-                # define column types
-                formats = ([(np.float32)] * (len(header_dbscan)))
-                formats[0] = (np.uint32)
-                formats[11] = (np.uint32)
-                formats[12] = (np.uint32)
-                formats[14] = (int)
-                ds_dt = np.dtype({'names': header_dbscan, 'formats': formats})
-                locs_file.create_dataset("locs", data=p1_dbscan_filtered, dtype=ds_dt)
+        with h5py.File(os.path.join(self.path, str(name)), "w") as locs_file:
+            ds_dt = np.dtype({'names': column_names.to_list(), 'formats': formats})
+            locs_file.create_dataset("locs", data=p1_dbscan_filtered, dtype=ds_dt)
+
 
     def save_yaml(self):
         name = self.filename + "_" + appendix + ".yaml"
         content = []
-        if x_coor == "x":
+        if "x" == "x":
             # save yaml file to reopen modified dbscan file
             with open(os.path.join(self.path, self.filename + ".yaml"), 'r') as yaml_file:
                 text = _yaml.load_all(yaml_file, _yaml.FullLoader)
@@ -131,12 +134,13 @@ class Save(object):
         df = pd.DataFrame(polygon, columns=["x", "y"])
         writer = pd.ExcelWriter(os.path.join(self.path, name), engine='xlsxwriter')
         df.to_excel(writer, sheet_name=appendix, index=False)
-        writer.save()
+        writer.close()
         pass
 
     def main(self):
         # Check for datatype and save as hdf5 file
-        self.save_pd_to_hdf5()
+        formats = self.getformats()
+        self.save_pd_to_hdf5(formats)
         self.save_yaml()
         self.save_mask()
 
@@ -144,17 +148,20 @@ class Save(object):
 # Load dbscan files
 HDF5_file = LoadHDF5(os.path.join(path, (file_name + ".hdf5")))  # load dbscan files
 HDF5_file_pd = HDF5_file.load()
+column_names = HDF5_file_pd.columns
 
-# check if dbscan or dbscluster file is loaded
-if "x" in HDF5_file_pd:
-    x_coor = "x"
-    y_coor = "y"
-else:
-    x_coor = "com_x"
-    y_coor = "com_y"
+
+
+# # check if dbscan or dbscluster file is loaded
+# if "x" in HDF5_file_pd:
+#     x_coor = "x"
+#     y_coor = "y"
+# else:
+#     x_coor = "com_x"
+#     y_coor = "com_y"
 
 fig, ax = plt.subplots(constrained_layout=True)
-ax.scatter(x=HDF5_file_pd[x_coor], y=HDF5_file_pd[y_coor], s=0.2, color="black", label="locs")
+ax.scatter(x=HDF5_file_pd["x"], y=HDF5_file_pd["y"], s=0.2, color="black", label="locs")
 plt.text(0, 0, "left: place point")
 plt.text(0, 10, "right: remove point")
 plt.text(0, 20, "scroll to zoom")
@@ -192,7 +199,7 @@ def check_locs_in_ROI(x, y, polygon):
     return inside
 
 
-points = np.stack((HDF5_file_pd[x_coor].to_numpy(), HDF5_file_pd[y_coor].to_numpy()), axis=1)
+points = np.stack((HDF5_file_pd["x"].to_numpy(), HDF5_file_pd["y"].to_numpy()), axis=1)
 points_inside = [check_locs_in_ROI(point[0], point[1], polygon) for point in points]
 if Mask == "True":
     points_outside = [not elem for elem in points_inside]  # reverse to get locs out of the ROI (Mask the region)
@@ -203,7 +210,7 @@ if Mask == "False":
 
 # Plot the data
 fig, ax = plt.subplots(constrained_layout=True)
-ax.scatter(x=p1_dbscan_pd_filter[x_coor], y=p1_dbscan_pd_filter[y_coor], s=0.2, color="blue", label="filter")
+ax.scatter(x=p1_dbscan_pd_filter["x"], y=p1_dbscan_pd_filter["y"], s=0.2, color="blue", label="filter")
 ax.plot(polygon[:, 0], polygon[:, 1], '--ko')
 ax.plot((polygon[0, 0], polygon[-1, 0]), (polygon[0, 1], polygon[-1, 1]), "--ko")
 plt.show()
